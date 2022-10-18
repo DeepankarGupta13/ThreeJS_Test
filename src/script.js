02d09daaa964d5779974fd1fa5632cecb1e390d4
Deepankar <deepankarsama@gmail.com>
2022-10-18 15:57:16 +0530

    added dxfjs library

--- src/script.js
+++ src/script.js
@@ -1,40 +1,94 @@
-import './style.css'
-import * as THREE from 'three'
+import {
+    DxfWriter,
+    point3d,
+    HatchPolylineBoundary,
+    HatchEdgesTypeData,
+    point2d,
+    vertex,
+    HatchBoundaryPaths,
+    pattern,
+    HatchPredefinedPatterns,
+} from "@tarikjabiri/dxf";
+import { saveAs } from 'file-saver';
+import { Colors, LayerFlags } from "@tarikjabiri/dxf";
 
-// Canvas
-const canvas = document.querySelector('#c')
+const dxf = new DxfWriter();
 
-// Scene
-const scene = new THREE.Scene()
+function main() {
+    drawText();
+    // To get the dxf string just call the stringify() method
+    const dxfString = dxf.stringify();
+    save(dxfString);
+}
 
-/**
- * Object
- */
-const geometry = new THREE.BoxGeometry(1, 1, 1)
-const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
-const mesh = new THREE.Mesh(geometry, material)
-scene.add(mesh)
+function drawText() {
+    const text = dxf.addText(point3d(20, 20, 0), 10, 'GGWP')
+    console.log('text: ', text);
+}
 
-/**
- * Sizes
- */
-const sizes = {
-    width: 800,
-    height: 600
-        }
+function drawDimension() {
+    const dim = dxf.addAlignedDim(point3d(0, 0, 0), point3d(100, 100, 0))
+    console.log('dim: ', dim);
+}
 
-/**
- * Camera
- */
-const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
-camera.position.z = 3
-scene.add(camera)
+function drawCircle() {
+    // dxf.addCircle(point3d(0, 0, 0), 10);
+    // or
+    const layer = dxf.addLayer("hey there", Colors.Red, "Continuous");
+    dxf.addLayer("YashRao", Colors.Green, "Continuous");
+    dxf.addCircle(point3d(0, 0, 0), 5, {layerName: 'hey there'});
+    const options = {
+        layerName : 'hey there',
+    }
+    const myCircle = dxf.addCircle(point3d(0, 0, 0), 10, options);
+    const myBlock = dxf.addBlock("myBlock");
+    myBlock.addCircle(point3d(0, 0, 0), 20).layerName = 'YashRao';
+    myBlock.addLine(point3d(0, 0, 0), point3d(0, 20, 0)).layerName = 'YashRao';
+    console.log('myBlock: ', myBlock);
 
-/**
- * Renderer
- */
-const renderer = new THREE.WebGLRenderer({
-    canvas: canvas
-})
-renderer.setSize(sizes.width, sizes.height)
-renderer.render(scene, camera)
+    // Inserting the block
+    dxf.addInsert(myBlock.name, point3d(0, 0, 0));
+    myCircle.layerName = 'hey there'
+    console.log('HEYY THERE',  myCircle.layerName);
+    console.log('myCircle: ', myCircle);
+    const gg = (options === null || options === void 0 ? void 0 : options.layerName) || '0'
+    console.log('gg: ', gg);
+    console.log('type of gg: ', typeof(gg));
+}
+
+function drawHatch() {
+    const polyline = new HatchPolylineBoundary();
+    polyline.add(vertex(0, 0));
+    polyline.add(vertex(0, 10000));
+    polyline.add(vertex(10000, 10000));
+    polyline.add(vertex(10000, 0));
+
+    const edges = new HatchEdgesTypeData();
+    edges.addLineEdgeData(point2d(0, 0), point2d(0, 10000));
+    edges.addLineEdgeData(point2d(0, 10000), point2d(10000, 10000));
+    edges.addLineEdgeData(point2d(10000, 10000), point2d(10000, 0));
+    edges.addLineEdgeData(point2d(10000, 0), point2d(0, 0));
+
+    const boundary = new HatchBoundaryPaths();
+    // Add the defined path
+    boundary.addPolylineBoundary(polyline);
+
+    const mysolid = pattern({
+        name: HatchPredefinedPatterns.ANSI31,
+        // Other properties you can define optionally
+        // angle?: number;
+        // scale?: number;
+        // double?: boolean;
+    });
+
+    const hatch = dxf.addHatch(boundary, mysolid);
+    console.log('hatch: ', hatch);
+}
+
+function save(dxfString) {
+    // console.log('dxfString: ', dxfString);
+    const blobData = new Blob([dxfString], { type: 'text/plain' });
+    saveAs(blobData, `test.dxf`);
+}
+
+main();
